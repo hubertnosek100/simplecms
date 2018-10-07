36877ebfa1f45c0d6e822634a9acdc9f71bf8b74
Hubert <hubert.nosek@gmail.com>
2018-10-07 15:08:43 +0200

    keys for edit

--- README.md
+++ README.md
@@ -6,3 +6,15 @@
 
 Login: admin
 Password: admin
+
+
+## Editing
+In html folder is example how to use the simplecms with simple components.
+You have to include simplecms.min.js and simplecms.min.css and run script described below.
+
+```
+t: simplecms.init("YOUR_SIMPLECMS_URL", "YOUR_API_KEY")
+e: simplecms.init("http://localhost:3000", "85f1f9772b69f4e8762b252de9c3cafabaf6ac04")
+```
+
+For edit component just press ctrl + shift + e for edit on your website where you imported the library.
--- html/simplecms.js
+++ html/simplecms.js
@@ -1240,7 +1240,44 @@ app.simpletext = (function () {
 
     function _load(url) {
         app.service.get(url + "/" + app.static.simpletext, _set);
-        _init();
+        _listenForCombination();
+    }
+
+    function _listenForCombination(params) {
+        var keys = {
+            shift: false,
+            ctrl: false,
+            e: false
+        };
+        var done = false;
+        $(document.body).keydown(function (e) {
+            // console.log(e.key)
+            // console.log(e.keyCode)
+            if (e.keyCode == 16) {
+                keys["shift"] = true;
+            } else if (e.keyCode == 17) {
+                keys["ctrl"] = true;
+            } else if (e.keyCode == 69) {
+                keys["e"] = true;
+            }
+            if (keys["shift"] && keys["ctrl"] && keys["e"]) {
+                if (!done) {
+                    done = true
+                    _init();
+                }
+            }
+        });
+
+        $(document.body).keyup(function (e) {
+            // reset status of the button 'released' == 'false'
+            if (e.keyCode == 16) {
+                keys["shift"] = false;
+            } else if (e.keyCode == 17) {
+                keys["ctrl"] = false;
+            } else if (e.keyCode == 69) {
+                keys["e"] = false;
+            }
+        });
     }
 
     function _init() {
