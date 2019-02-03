62ef20274b9d355f2d73c669e88eaa1feea2ab30
Hubert Nosek <hubert.nosek@gmail.com>
2019-02-03 16:48:41 +0100

    env openshit

--- server.js
+++ server.js
@@ -37,10 +37,12 @@ server.use((req, res, next) => {
   }
 })
 server.use(router)
-var port = 3000;
-server.listen(port, () => {
-  console.log('JSON Server is running on port: ' + port)
-})
+var port = 8080;
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 
-
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
