87d15697cf1c4f562e6358c42cdb1e30517a4abe
Hubert Nosek <hubert.nosek@gmail.com>
2019-02-03 17:37:26 +0100

    ip changed

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ server.use(router)
 var port = 8080;
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 
 server.listen(server_port, server_ip_address, function () {
